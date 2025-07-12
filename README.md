d13eb63ff7ae3ae343ab0ab11258a54ced955adf
agimkamberi <agim.kamberi@seavus.com>
2025-07-12 22:15:51 +0200

    Populate the app

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ynet
 Y-Net APP (PHP-Javascript-MySQL)
+git remote set-url origin https://agim89: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@github.com/agim89/ynet.git
---
+++ radiusmanager/bkash/api-handle.php
@@ -0,0 +1,19 @@
+<?php //0046a
+if(!extension_loaded('ionCube Loader')){$__oc=strtolower(substr(php_uname(),0,3));$__ln='ioncube_loader_'.$__oc.'_'.substr(phpversion(),0,3).(($__oc=='win')?'.dll':'.so');if(function_exists('dl')){@dl($__ln);}if(function_exists('_il_exec')){return _il_exec();}$__ln='/ioncube/'.$__ln;$__oid=$__id=realpath(ini_get('extension_dir'));$__here=dirname(__FILE__);if(strlen($__id)>1&&$__id[1]==':'){$__id=str_replace('\\','/',substr($__id,2));$__here=str_replace('\\','/',substr($__here,2));}$__rd=str_repeat('/..',substr_count($__id,'/')).$__here.'/';$__i=strlen($__rd);while($__i--){if($__rd[$__i]=='/'){$__lp=substr($__rd,0,$__i).$__ln;if(file_exists($__oid.$__lp)){$__ln=$__lp;break;}}}if(function_exists('dl')){@dl($__ln);}}else{die('The file '.__FILE__." is corrupted.\n");}if(function_exists('_il_exec')){return _il_exec();}echo('Site error: the file <b>'.__FILE__.'</b> requires the ionCube PHP Loader '.basename($__ln).' to be installed by the website operator. If you are the website operator please use the <a href="http://www.ioncube.com/lw/">ionCube Loader Wizard</a> to assist with installation.');exit(199);
+?>
+HR+cPmKcw7wMyD1nD9rzNOyTKgZ4SaeP0tC5yk3E74TSkqIaV7vXzT7P50SZ44ZoL31Cy0SSKdb1
+MG0DacWAQXTocPDr7CUlEeBW7l8r9TULkt642M7K+OmlyzasBsy9MIkKKYEyfgnHSRcDqUvnEvr+
+5x2KV9WXLMgl4iVmFhGvpArS1wNfnbbotLHklquwjz0+BP1y6PtQrLBM8dqFhA6c+7TzrA65tUmk
+Q7b02bIFe1flf7lpMId3O61xkQoH7Y75aHIQzFcCaz3syJLy4ctP0AuIXJsPMHA+OywnoGhtRqxN
+Lds+ZtUnoSQB54dnXHEYp0G5G4wSfd5mVC4kd6t7a7hlD3/CYIsMgq9E1i/72JZ/TbkjSMlt4NQB
+KLbjwFUpAScr9swwmucVObL0Q0TVguT1wj5FmP75kcf9QWLj7CvvvUz5oc/fcH7YAyQZS5/RDbgO
+tKdpr64zCqKN/mqd/OcVs0/CvBNKHi7TA5H2cRq8wFyf8Un8Opy4irMJEcvkcpqORDOrfFprl2zh
+m3W8CNTwFGOVZLbdXudXs7r7GpyVxkOQpyt/R38qA1Y1T/sA6TVKZvYsSHbSggxct/HFghKFFcPV
+LmM3x1/OfpPDbHlD0+fgPwoSIzpckyYfB8ud7kNhbyVXWX7aIK7LNXs4lLiNzwkPzL2wdNznl0/A
+32F/xBoNo0x6eN1q6PWdaZl/LWXyekaqYi9Ep9RpCFQXTwR5wUSgOBC2PvpDsSxPymDb4TvADT1A
+5RcNlaJDiD0A8dWXAxWS8C8ts4tnvUwR0eAQZCS7PFYgVUxlLuPqNWh3g7eTdD0DmCTYD0sMgt5l
+MVF5raGqGP5iThl1XxOXEpxXyrnsmH0NKuYU7PFobnbCCIyPNFOVzPMo/k4FoHULEPO/a9aS58SB
+hZBjjf7DHgMk+x0eDHzQBant9hQriWCPOND+BxjrGVVOQ/XRdC5BpI+agiuOZlam6ZIF/9RlwoOp
+k8sHkq4GdYxAlf1G5ix+yZMXl5lY5H35/Fjcy+ZW9MKYuRKMDcFLWaEQQAbrtDYgBZvh9qhcRBxT
+SI16LKoy6Rx39LnGVnwHTvEcFq3XyMW0wZVRnTPVP7b+Ouk9EnUvMzf5BdpG+05mH0rZQctTycZ4
+kGKrrxxK9uym
